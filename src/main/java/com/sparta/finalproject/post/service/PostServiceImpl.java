171c3775bc13c2c1d64f520838bc415cf23b0fd0
2h5i <dev.2h5i@gmail.com>
2023-02-08 14:12:48 +0900

    [feat] post service에 @Transactional 추가

--- src/main/java/com/sparta/finalproject/post/service/PostServiceImpl.java
+++ src/main/java/com/sparta/finalproject/post/service/PostServiceImpl.java
@@ -7,6 +7,7 @@ import com.sparta.finalproject.post.repository.PostRepository;
 import com.sparta.finalproject.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,7 @@ public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
 
     @Override
+    @Transactional
     public Long createPost(PostDto.CreatePost createPost, User user) {
         Post post = Post.builder()
             .title(createPost.getTitle())
@@ -28,6 +30,7 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public PostDto.ResponsePost getPostById(Long postId) {
         Post post = postRepository.findById(postId).orElseThrow(
             () -> new BadRequestException("해당하는 게시물이 없습니다.")
