355d03690755ede5436ec7e2d18917c5416c858b
sung <zoo5607@naver.com>
2023-02-09 08:31:21 +0900

    [feat] Oauth 기능 추가

--- src/main/java/com/sparta/finalproject/user/repository/UserRepository.java
+++ src/main/java/com/sparta/finalproject/user/repository/UserRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByUserId(String userId);
-  
+  Optional<User> findBykakaoId(Long id);
+  Optional<User> findByEmail(String email);
+
 }
