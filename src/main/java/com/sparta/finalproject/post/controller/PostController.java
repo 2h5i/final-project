28da3eb69823c6a77a0d80cf3e71cbe41ba9f9cc
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-02-10 10:12:41 +0900

    Revert "Revert "Merge branch 'develop' into final-project-14""
    This reverts commit f60a8a36f32044220d398d8f202bddb0014101b6.

--- src/main/java/com/sparta/finalproject/post/controller/PostController.java
+++ src/main/java/com/sparta/finalproject/post/controller/PostController.java
@@ -1,10 +1,14 @@
 package com.sparta.finalproject.post.controller;
 
+import com.sparta.finalproject.common.core.PageWrapper;
 import com.sparta.finalproject.common.security.UserDetailsImpl;
 import com.sparta.finalproject.post.dto.PostDto;
+import com.sparta.finalproject.post.dto.PostDto.SearchPost;
 import com.sparta.finalproject.post.service.PostService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -40,6 +44,13 @@ public class PostController {
         return postService.getPostById(postId);
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public PageWrapper getPosts(SearchPost searchPost,
+        @PageableDefault() Pageable pageable) {
+        return PageWrapper.of(postService.getPosts(pageable, searchPost));
+    }
+
     @PutMapping("/{postId}")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('ROLE_USER')")
@@ -58,6 +69,5 @@ public class PostController {
 
         postService.deletePost(postId, userDetails.getUser());
     }
-
 
 }
