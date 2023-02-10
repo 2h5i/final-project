74720d0513bbac0473cec8230783669f9d3be0e2
2h5i <dev.2h5i@gmail.com>
2023-02-10 09:44:27 +0900

    [feat] 게시글 전체 검색 조회 개발

--- src/main/java/com/sparta/finalproject/post/repository/PostCustomRepository.java
+++ src/main/java/com/sparta/finalproject/post/repository/PostCustomRepository.java
@@ -1,5 +1,11 @@
 package com.sparta.finalproject.post.repository;
 
+import com.sparta.finalproject.post.dto.PostDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PostCustomRepository {
 
+    Page<PostDto.ResponsePost> getPostsBySearchCondition(Pageable pageable,
+        PostDto.SearchPost searchPost);
 }
--- src/main/java/com/sparta/finalproject/post/repository/PostCustomRepositoryImpl.java
+++ src/main/java/com/sparta/finalproject/post/repository/PostCustomRepositoryImpl.java
@@ -1,5 +1,59 @@
 package com.sparta.finalproject.post.repository;
 
-public class PostCustomRepositoryImpl implements PostCustomRepository {
+import static com.sparta.finalproject.post.entity.QPost.post;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.finalproject.post.dto.PostDto;
+import com.sparta.finalproject.post.dto.PostDto.ResponsePost;
+import com.sparta.finalproject.post.entity.Post;
+import java.util.List;
+import java.util.Objects;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PostCustomRepositoryImpl extends QuerydslRepositorySupport implements
+    PostCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public PostCustomRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
+        super(Post.class);
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    private BooleanExpression searchByTitle(String title) {
+        return Objects.nonNull(title) ? post.title.contains(title) : null;
+    }
+
+    private BooleanExpression searchByContent(String content) {
+        return Objects.nonNull(content) ? post.content.contains(content) : null;
+    }
+
+    @Override
+    public Page<ResponsePost> getPostsBySearchCondition(Pageable pageable,
+        PostDto.SearchPost searchPost) {
+        List<Post> posts = jpaQueryFactory.selectFrom(post)
+            .where(
+                searchByTitle(searchPost.getTitle()),
+                searchByContent(searchPost.getContent())
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        Long postCount = jpaQueryFactory.select(Wildcard.count)
+            .from(post)
+            .where(
+                searchByTitle(searchPost.getTitle()),
+                searchByContent(searchPost.getContent())
+            ).fetch().get(0);
+
+        return new PageImpl<>(ResponsePost.of(posts), pageable, postCount);
+    }
 }
