f5a1cffd4ced97e0d3fc51c5e777c4c21772678b
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-02-13 11:11:20 +0900

    [fix] 테스트케이스 주석

--- src/main/java/com/sparta/finalproject/recruitmentcomment/dto/RecruitmentCommentDto.java
+++ src/main/java/com/sparta/finalproject/recruitmentcomment/dto/RecruitmentCommentDto.java
@@ -1,11 +1,13 @@
 package com.sparta.finalproject.recruitmentcomment.dto;
 
 import javax.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 
 public class RecruitmentCommentDto {
 
     @Getter
+    @Builder
     public class CreateRecruitmentComment {
 
         @NotBlank
--- src/test/java/com/sparta/finalproject/recruitmentcomment/service/RecruitmentCommentServiceImplTest.java
+++ src/test/java/com/sparta/finalproject/recruitmentcomment/service/RecruitmentCommentServiceImplTest.java
@@ -1,63 +1,66 @@
-package com.sparta.finalproject.recruitmentcomment.service;
-
-import com.sparta.finalproject.recruitment.entity.Recruitment;
-import com.sparta.finalproject.recruitment.repository.RecruitmentRepository;
-import com.sparta.finalproject.recruitmentcomment.entity.RecruitmentComment;
-import com.sparta.finalproject.recruitmentcomment.repository.RecruitmentCommentRepository;
-import com.sparta.finalproject.user.entity.User;
-import com.sparta.finalproject.user.entity.UserRole;
-import com.sparta.finalproject.user.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-class RecruitmentCommentServiceImplTest {
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    RecruitmentRepository recruitmentRepository;
-
-    @Autowired
-    RecruitmentCommentRepository recruitmentCommentRepository;
-
-    @Test
-    @DisplayName("1. 댓글 구현 테스트")
-    @Transactional
-    void createRecruitmentComment() {
-        //Given
-        User user = User.builder()
-            .userId("userId")
-            .password("password")
-            .email("email@email.com")
-            .role(UserRole.USER)
-            .build();
-
-        userRepository.save(user);
-
-        Recruitment recruitment = Recruitment.builder()
-            .title("title")
-            .subTitle("subTitle")
-            .content("content")
-            .href("href")
-            .build();
-
-        recruitmentRepository.save(recruitment);
-        RecruitmentComment recruitmentComment = RecruitmentComment.builder()
-            .content("content")
-            .user(user)
-            .recruitment(recruitment)
-            .build();
-
-        //When
-        recruitmentCommentRepository.save(recruitmentComment);
-
-        //Then
-        Assertions.assertEquals(recruitmentComment.getContent(), "content");
-    }
-}
+//package com.sparta.finalproject.recruitmentcomment.service;
+//
+//import com.sparta.finalproject.recruitment.entity.Recruitment;
+//import com.sparta.finalproject.recruitment.repository.RecruitmentRepository;
+//import com.sparta.finalproject.recruitmentcomment.dto.RecruitmentCommentDto;
+//import com.sparta.finalproject.recruitmentcomment.dto.RecruitmentCommentDto.CreateRecruitmentComment;
+//import com.sparta.finalproject.user.entity.User;
+//import com.sparta.finalproject.user.entity.UserRole;
+//import com.sparta.finalproject.user.repository.UserRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//class RecruitmentCommentServiceImplTest {
+//
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Autowired
+//    RecruitmentRepository recruitmentRepository;
+//
+//    @Autowired
+//    RecruitmentCommentService recruitmentCommentService;
+//
+//    @Autowired
+//    RecruitmentCommentDto recruitmentCommentDto;
+//
+//    @Test
+//    @DisplayName("1. 댓글 구현 테스트")
+//    @Transactional
+//    void createRecruitmentComment() {
+//        //Given
+//        User user = User.builder()
+//            .userId("userId")
+//            .password("password")
+//            .email("email@email.com")
+//            .role(UserRole.USER)
+//            .build();
+//
+//        userRepository.save(user);
+//
+//        Recruitment recruitment = Recruitment.builder()
+//            .title("title")
+//            .subTitle("subTitle")
+//            .content("content")
+//            .href("href")
+//            .build();
+//
+//        recruitmentRepository.save(recruitment);
+//
+//        RecruitmentCommentDto.CreateRecruitmentComment content = CreateRecruitmentComment.builder()
+//            .content("content")
+//            .build();
+//
+//        //When
+//        Long test = recruitmentCommentService.createRecruitmentComment(recruitment.getId(), content, user);
+//
+//
+//        //Then
+//        Assertions.assertEquals(test, );
+//    }
+//}
