0912d9086e24a1182dc83ae2b451fab17428a796
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-02-14 15:39:52 +0900

    [refactor] url 수정

--- src/main/java/com/sparta/finalproject/bookmark/controller/BookmarkController.java
+++ src/main/java/com/sparta/finalproject/bookmark/controller/BookmarkController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/recruitments")
+@RequestMapping("/api/bookmarks")
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
