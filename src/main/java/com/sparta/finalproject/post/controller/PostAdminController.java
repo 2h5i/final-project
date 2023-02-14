76adc2f81fa951235be20455c9ae6c3958c5657a
2h5i <dev.2h5i@gmail.com>
2023-02-14 19:16:47 +0900

    [feat] 관리자 자유게시판 단건 조회

--- src/main/java/com/sparta/finalproject/post/controller/PostAdminController.java
+++ src/main/java/com/sparta/finalproject/post/controller/PostAdminController.java
@@ -1,10 +1,12 @@
 package com.sparta.finalproject.post.controller;
 
+import com.sparta.finalproject.post.dto.PostDto.ResponsePost;
 import com.sparta.finalproject.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostAdminController {
 
     private final PostService postService;
+
+    @GetMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponsePost selectPostAdmin(@PathVariable Long postId) {
+        return postService.getPostById(postId);
+    }
 
     @DeleteMapping("/{postId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
