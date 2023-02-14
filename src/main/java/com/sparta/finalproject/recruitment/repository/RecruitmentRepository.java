794357999a0a7671a62ea9793a9a99726a97be80
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-02-14 09:24:43 +0900

    [refactor] 미사용 코드 삭제

--- src/main/java/com/sparta/finalproject/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/sparta/finalproject/recruitment/repository/RecruitmentRepository.java
@@ -1,12 +1,9 @@
 package com.sparta.finalproject.recruitment.repository;
 
 import com.sparta.finalproject.recruitment.entity.Recruitment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long>,
     RecruitmentCustomRepository {
 
-    Page<Recruitment> findById(long id, Pageable pageable);
 }
