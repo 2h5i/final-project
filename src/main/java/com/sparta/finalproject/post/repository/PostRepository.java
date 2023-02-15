c37d8b0c34c7b5f6944d1fc4a2ead4e650793d84
2h5i <dev.2h5i@gmail.com>
2023-02-15 10:48:10 +0900

    [refactor] clear git cache

--- src/main/java/com/sparta/finalproject/post/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.finalproject.post.repository;
-
-import com.sparta.finalproject.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
-
-    
-}
