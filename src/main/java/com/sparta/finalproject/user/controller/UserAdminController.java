218c1090224519de7695e295955805879e74880d
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-02-16 14:16:38 +0900

    [refactor] 오타수정

--- src/main/java/com/sparta/finalproject/user/controller/UserAdminController.java
+++ src/main/java/com/sparta/finalproject/user/controller/UserAdminController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/users")
 @PreAuthorize("hasRole('ROLE_ADMIN')")
-public class UserAdminComtroller {
+public class UserAdminController {
 
     private final UserService userservice;
 
