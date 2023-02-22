d7d88ec4c7fb28b2e8823c6b30466995b0562dca
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-02-22 18:12:29 +0900

    [refactor] 주석 제거

--- src/main/java/com/sparta/finalproject/auth/controller/AuthController.java
+++ src/main/java/com/sparta/finalproject/auth/controller/AuthController.java
@@ -18,29 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-  private final AuthService authService;
+    private final AuthService authService;
 
-  @PostMapping("/signup")
-  public void signup(@RequestBody @Valid AuthDto.SignupDto signupDto) {
-    authService.signup(signupDto);
-  }
+    @PostMapping("/signup")
+    public void signup(@RequestBody @Valid AuthDto.SignupDto signupDto) {
+        authService.signup(signupDto);
+    }
 
-  @PostMapping("/login")
-  public void login(@RequestBody AuthDto.LoginDto loginDto, HttpServletResponse response) {
-    authService.login(loginDto, response);
-  }
+    @PostMapping("/login")
+    public void login(@RequestBody AuthDto.LoginDto loginDto, HttpServletResponse response) {
+        authService.login(loginDto, response);
+    }
 
-  @GetMapping("/kakao-login")
-  public String kakaoLogin(@RequestParam String code, HttpServletResponse response)
-      throws JsonProcessingException {
-    // code: 카카오 서버로부터 받은 인가 코드
-    String createToken = authService.kakaoLogin(code, response);
+    @GetMapping("/kakao-login")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response)
+        throws JsonProcessingException {
+        // code: 카카오 서버로부터 받은 인가 코드
+        String createToken = authService.kakaoLogin(code, response);
 
-    return createToken;
-
-    // Cookie 생성 및 직접 브라우저에 Set
-//    Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
-//    cookie.setPath("/");
-//    response.addCookie(cookie);
-  }
+        return createToken;
+    }
 }
